a2075da31889bce3638471ad0c9f6e9e447b279f
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-09 14:28:02 +0300

    a

--- working_with_html/parse_html.py
+++ working_with_html/parse_html.py
@@ -1,13 +1,26 @@
 import urllib.request
 import ssl
+import requests
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+class HtmlReader:
+    def HtmlReaderBad(self, url, output_filename):
+        # Ignore SSL certificate errors
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        html = urllib.request.urlopen(url, context=ctx).read()
+        fhand = open(output_filename+'.html', 'wb')
+        fhand.write(html)
+        fhand.close()
+
+
+    def HtmlReaderGood(self, url, output_filename):
+        r = requests.get(url)
+        with open(output_filename+'.html', 'w') as output_file:
+            output_file.write(r.text)
+
 
 url = 'https://www.cbr.ru/currency_base/daily/'
-html = urllib.request.urlopen(url, context=ctx).read()
-fhand = open('copy_of_the_page.html', 'wb')
-fhand.write(html)
-fhand.close()
+html_reader = HtmlReader
+html_reader.HtmlReaderGood(html_reader, url, 'copy_page')
