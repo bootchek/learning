55cd98b81ff20ede41f6c397fd1152a8e65e8b2f
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-14 11:57:46 +0300

    вывод на экран курса и сохранение в json

--- working_with_html/get_currency/main.py
+++ working_with_html/get_currency/main.py
@@ -5,12 +5,13 @@ from currency import Currency
 import json_creator
 
 url = 'https://www.cbr.ru/currency_base/daily/'
-input_file = open(sys.argv[1], "r")
 
 while True:
+    input_file = open(sys.argv[1], "r")
     json_list = []
     read_page = get_data.readhtml_good(url)
-    for code in input_file:
+    for line in input_file:
+        code = line
         code = code.rstrip('\n')
         quantity = get_data.get_currency_parametr_from_column(read_page, code, 5)
         name = get_data.get_currency_parametr_from_column(read_page, code, 7)
@@ -18,9 +19,9 @@ while True:
         json_list.append(Currency(code, quantity, name, value))
         print(code, " равен ", value)
     output_json = json_creator.create_json_from(json_list)
-    output_file = open(sys.argv[2] + 'output.json', 'w')
+    output_file = open(sys.argv[2], 'w')
     output_file.write(output_json)
     output_file.close()
     print('\n')
 
-    time.sleep(10)
+    time.sleep(1)
