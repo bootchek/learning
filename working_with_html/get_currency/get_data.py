4a47de181f3566bfca99f537e1cf631814fbeb99
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-14 18:28:53 +0300

    сначала читаем весь html, затем вытаскиваем требуемые валюты

--- working_with_html/get_currency/get_data.py
+++ working_with_html/get_currency/get_data.py
@@ -1,19 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from currency import Currency
 
 def readhtml_good(url):
     return requests.get(url).text
 
 
-def get_currency_parametr_from_column(html, currency_code, column):
+def get_currency_data(html):
     """Из таблицы вытаскиваем требуемый параметр для конкретной валюты.
     currency_data - номер столбца в таблице, где расположен требуемый параметр."""
 
     soup = BeautifulSoup(html, 'html.parser')
     divs = soup.find_all('div')
+    currencies_with_data = []
     for div in divs:
         if 'class' in div.attrs:
             if div['class'] == ["table-wrapper"]:
                 for currency in div.find_all('tr'):
-                    if currency.contents[3].string == currency_code:
-                        return currency.contents[column].string
+                    code = currency.contents[3].string
+                    quantity = currency.contents[5].string
+                    name = currency.contents[7].string
+                    value = currency.contents[9].string
+                    currencies_with_data.append(Currency(code, quantity, name, value))
+    return currencies_with_data
