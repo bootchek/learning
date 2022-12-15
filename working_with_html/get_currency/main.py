a399609d630c8d0e29cff4c795f8b8b384e43c1d
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-15 10:42:49 +0300

    подкорректировал названия переменных

--- working_with_html/get_currency/main.py
+++ working_with_html/get_currency/main.py
@@ -1,7 +1,6 @@
 import time
 import sys
 import get_data
-from currency import Currency
 import json_creator
 
 url = 'https://www.cbr.ru/currency_base/daily/'
@@ -10,14 +9,14 @@ codes = input_file.read().splitlines()
 input_file.close()
 
 while True:
-    json_list = []
-    read_page = get_data.readhtml_good(url)
-    data_from_page = get_data.get_currency_data(read_page)
+    currencies = []
+    html_string = get_data.readhtml_good(url)
+    data_from_page = get_data.get_currency_data(html_string)
     for currency in data_from_page:
         if currency.code in codes:
-            json_list.append(currency)
+            currencies.append(currency)
             print(currency.code, " равен ", currency.value)
-    output_json = json_creator.create_json_from(json_list)
+    output_json = json_creator.create_json_from(currencies)
     output_file = open(sys.argv[2], 'w')
     output_file.write(output_json)
     output_file.close()
