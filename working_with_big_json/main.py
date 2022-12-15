f579d411b5062ebbfd6de8df9773956b91689fc6
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-15 16:05:45 +0300

    новый файл: дробим большой json

---
+++ working_with_big_json/main.py
@@ -0,0 +1,18 @@
+import sys
+import json
+
+f = open(sys.argv[1])
+
+k, n, i = sys.argv[2], 0, 0
+data = json.load(f)
+while i < len(data) - 1:
+    json_list = []
+    for i in range(i, i + int(k)):
+        if i == len(data):
+            break
+        else:
+            json_list.append(data[i])
+    n += 1
+    i += 1
+    with open(sys.argv[3]+"file_" + str(n) + ".json", "w") as outfile:
+        json.dump(json_list, outfile, indent=2)
