91406c5e15eb4a3da94c261a9bb3ac27a9626e66
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-20 10:47:59 +0300

    скачивание файла и запись его в csv теперь функция

--- working_with_csv/main.py
+++ working_with_csv/main.py
@@ -6,20 +6,25 @@ import csv
 
 url = "https://raw.githubusercontent.com/jokecamp/FootballData/master/World%20Cups/all-world-cup-players.json"
 
-with tempfile.TemporaryDirectory() as tmpdirname:  # создаём временную директорию; она существует, пока мы в блоке with
-    # сохраняем файл в этой директории а вот эта строка должна будет конфликтовать с предыдущим коммитом
-    # url_data = requests.get(sys.argv[1])
-    url_data = requests.get(url)
-    with open(tmpdirname + '/local.json', 'wb') as file:
-        file.write(url_data.content)
-    # #открываем файл и работаем с ним
-    f = open(tmpdirname + '/local.json', 'r')
-    json_data = json.load(f)
-    f.close()
-    attributes = json_data[0].keys()
-    with open('local.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(json_data[0].keys())
-        for object in json_data:
-            writer.writerow([object[attribute] for attribute in attributes]) #вот тут я использую list comprehension
-    f.close()
+
+def first_function(url, outputfile_location):
+    with tempfile.TemporaryDirectory() as tmpdirname:  # создаём временную директорию; она существует, пока мы в блоке with
+        url_data = requests.get(sys.argv[1])
+        # url_data = requests.get(url)
+        with open(tmpdirname + '/local.json', 'wb') as file:  # сохраняем файл во временной директории
+            file.write(url_data.content)
+        f = open(tmpdirname + '/local.json', 'r')  # открываем файл и работаем с ним
+        json_data = json.load(f)
+        f.close()
+        attributes = json_data[0].keys()  # получаем массив с названиями атрибутов каждого объекта из файла json
+        with open(outputfile_location, 'w', newline='') as f:  # создаём csv-файл
+            writer = csv.writer(f)
+            writer.writerow(attributes)  # в первую строку записываем названия атрибутов
+            for object in json_data:  # в следующие строки записываем значения атрибутов для каждого объекта
+                writer.writerow(
+                    [object[attribute] for attribute in attributes])  # вот тут я использую list comprehension
+                # для прохождения по каждому атрибуту
+        f.close()
+
+
+first_function(url, "/Users/danilbuslaev/Desktop/local.csv")
