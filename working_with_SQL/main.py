a482c006ddcc6292f47518eb60ec4fd27fede147
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-21 17:09:55 +0300

    добавляем поле в таблице

--- working_with_SQL/main.py
+++ working_with_SQL/main.py
@@ -1,18 +1,25 @@
 import sqlite3
+from datetime import datetime
 
 path = "/Users/danilbuslaev/Desktop/test_db"
 con = sqlite3.connect(path)  # создание БД и подключение к ней
 cur = con.cursor()  # "курсор" для отправки запросов и получения результатов
 
-cur.execute("CREATE TABLE IF NOT EXISTS Note(number INTEGER, content TEXT)")
-data = [(1, "Some text for note 1"),
-        (2, "Some text for note 2"),
-        (3, "Some text for note 3"),
-        (4, "Some text for note 4")]
+# cur.execute("CREATE TABLE IF NOT EXISTS Note(number INTEGER, content TEXT)")
+cur.execute("""ALTER TABLE Note
+  ADD created TEXT""")
 
-cur.executemany("INSERT INTO Note VALUES (?, ?)", data)
+now = datetime.now()
+current_time = now.strftime("%d/%m/%Y %H:%M:%S")
+
+data = [(5, "Some text for note 5", current_time),
+        (6, "Some text for note 6", current_time),
+        (7, "Some text for note 7", current_time),
+        (8, "Some text for note 8", current_time)]
+
+cur.executemany("INSERT INTO Note VALUES (?, ?, ?)", data)
 con.commit()
-res = cur.execute("SELECT number, content FROM Note")
+res = cur.execute("SELECT number, content, created FROM Note")
 notes_data = res.fetchall()
 for note in data:
     print(f"Note {note[0]} content: {note[1]}")
