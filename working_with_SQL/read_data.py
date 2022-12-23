cd4c92113c5a8a207ceae4541bccfd61331c4f5b
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-23 10:33:28 +0300

    разделил файлы по назначению

---
+++ working_with_SQL/read_data.py
@@ -0,0 +1,11 @@
+import sqlite3
+from datetime import datetime
+
+path = "/Users/danilbuslaev/Desktop/test_db"
+con = sqlite3.connect(path)  # создание БД и подключение к ней
+cur = con.cursor()  # "курсор" для отправки запросов и получения результатов
+
+res = cur.execute("SELECT number, content, created FROM Note")
+notes_data = res.fetchall()
+for note in notes_data:
+    print(f"Note {note[0]} content: {note[1]}")
--- working_with_SQL/write_data.py
+++ working_with_SQL/write_data.py
@@ -5,10 +5,6 @@ path = "/Users/danilbuslaev/Desktop/test_db"
 con = sqlite3.connect(path)  # создание БД и подключение к ней
 cur = con.cursor()  # "курсор" для отправки запросов и получения результатов
 
-# cur.execute("CREATE TABLE IF NOT EXISTS Note(number INTEGER, content TEXT)")
-cur.execute("""ALTER TABLE Note
-  ADD created TEXT""")
-
 now = datetime.now()
 current_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
@@ -19,7 +15,3 @@ data = [(5, "Some text for note 5", current_time),
 
 cur.executemany("INSERT INTO Note VALUES (?, ?, ?)", data)
 con.commit()
-res = cur.execute("SELECT number, content, created FROM Note")
-notes_data = res.fetchall()
-for note in data:
-    print(f"Note {note[0]} content: {note[1]}")
