4a2a3f0cc32b7a36decce281cc88aff34c39b724
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-23 12:55:21 +0300

    считываем питоном данные из таблицы; конвертируем время и дату

--- working_with_SQL/read_data.py
+++ working_with_SQL/read_data.py
@@ -1,5 +1,6 @@
 import sqlite3
 from datetime import datetime
+import re
 
 path = "/Users/danilbuslaev/Desktop/test_db"
 con = sqlite3.connect(path)  # создание БД и подключение к ней
@@ -8,4 +9,8 @@ cur = con.cursor()  # "курсор" для отправки запросов и
 res = cur.execute("SELECT number, content, created FROM Note")
 notes_data = res.fetchall()
 for note in notes_data:
-    print(f"Note {note[0]} date: {note[2][:10]}, time:{note[2][10:]}, content: {note[1]}")
+    match = re.search(r'\d{2}/\d{2}/\d{4}', note[2])
+    date = datetime.strptime(match.group(), '%d/%m/%Y').date()
+    match = re.search(r'\d{2}:\d{2}:\d{2}', note[2])
+    time = datetime.strptime(match.group(), '%H:%M:%S').time()
+    print(f"Note {note[0]} date: {date}, time:{time}, content: {note[1]}")
