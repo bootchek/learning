1114466405bc18eb469378d242faeac20a276f7d
bootchek <119874720+bootchek@users.noreply.github.com>
2022-12-27 14:22:00 +0300

    начинаем использовать шаблоны

--- working_with_web/flaskr/__init__.py
+++ working_with_web/flaskr/__init__.py
@@ -1,38 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 from tabulate import tabulate
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    conn = sqlite3.connect("/Users/danilbuslaev/Desktop/test_db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route("/")
 def hello_world():
-    path = "/Users/danilbuslaev/Desktop/test_db"
-    con = sqlite3.connect(path)  # создание БД и подключение к ней
-    cur = con.cursor()  # "курсор" для отправки запросов и получения результатов
-
-    res = cur.execute("""SELECT number, adress
+    conn = get_db_connection()
+    res = conn.execute("""SELECT number, adress
                             FROM flats
                             INNER JOIN buildings on buildings.id = flats.building_id;
                             """)
     list_of_flats = res.fetchall()
     table = tabulate(list_of_flats, tablefmt="html")
-    return table
+    conn.close()
+    return render_template('index.html', flats = list_of_flats)
 
 
 @app.route('/flats/<number>')
-def user(number):
-    path = "/Users/danilbuslaev/Desktop/test_db"
-    con = sqlite3.connect(path)  # создание БД и подключение к ней
-    cur = con.cursor()  # "курсор" для отправки запросов и получения результатов
-
-    res = cur.execute("""SELECT * FROM flats WHERE number = ?;
+def flat(number):
+    conn = get_db_connection()
+    res = conn.execute("""SELECT number FROM flats WHERE number = ?;
                             """, (number,))
-    list_of_flats = res.fetchall()
-    table = tabulate(list_of_flats, tablefmt="html")
-    return table
+    flat = res.fetchone()
+    conn.close()
+    return render_template('flat.html', number=flat)
 
 
 if __name__ == '__main__':
